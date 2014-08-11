221169917cac7dc1dcc901c67a0bba5604be5f0d
Jennifer Chen <cc2732@gmail.com>
2014-08-11 10:31:18 -0700

    Add seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,19 +1,19 @@
 ActiveRecord::Base.transaction do
 
-  # User.create!({ 
-  #   name: "Guest User", 
-  #   email: "guest@progressit.co", 
-  #   password: ENV['DEFAULT_PASSWORD'], 
-  # })
+  User.create!({ 
+    name: "Guest User", 
+    email: "guest@progressit.co", 
+    password: ENV['DEFAULT_PASSWORD'], 
+  })
 
-  # 10.times do
-  #   name = Faker::Name.name
-  #   email = Faker::Internet.email(name)
-  #   User.create!({ 
-  #     name: name, 
-  #     email: email, 
-  #     password: ENV['DEFAULT_PASSWORD'],
-  #   })
-  # end
+  20.times do
+    name = Faker::Name.name
+    email = Faker::Internet.email(name)
+    User.create!({ 
+      name: name, 
+      email: email, 
+      password: ENV['DEFAULT_PASSWORD'],
+    })
+  end
 
 end
